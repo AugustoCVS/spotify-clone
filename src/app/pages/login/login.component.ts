83d57a36a680e2f43b4889bc37e0607de0dc96cd
AugustoCVS <guty.vertolis@gmail.com>
2024-10-25 18:56:18 -0300

    feat: start to implemeanting spotify service on login component

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,8 +1,28 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { SpotifyService } from "../../services/spotify.service";
 
 @Component({
   selector: "app-login",
   templateUrl: "./login.component.html",
   styleUrls: ["./login.component.scss"]
 })
-export class LoginComponent { }
+export class LoginComponent implements OnInit {
+
+  constructor(private spotifyService: SpotifyService) { }
+
+  ngOnInit(): void {
+    this.verifyTokenUrl();
+  }
+
+  verifyTokenUrl(): void {
+    const token = this.spotifyService.getTokenFromUrlCallback();
+    if (!!token) {
+      this.spotifyService.defineAccessToken({ token });
+    }
+  }
+
+  openLoginPage() {
+    window.location.href = this.spotifyService.getLoginUrl();
+  }
+
+}

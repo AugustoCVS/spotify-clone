c37a53c6a18f4fc934ad981a729f9ef0dfc6cff4
AugustoCVS <guty.vertolis@gmail.com>
2024-10-29 17:48:55 -0300

    feat: implemeanting spotifyService and create getPlaylists

--- src/app/components/left-panell/left-panell.component.ts
+++ src/app/components/left-panell/left-panell.component.ts
@@ -1,12 +1,16 @@
 import { faGuitar, faHome, faMusic, faSearch } from '@fortawesome/free-solid-svg-icons';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IPlaylist } from '../../interfaces/playlist';
+import { SpotifyService } from '../../services/spotify.service';
 
 @Component({
   selector: 'app-left-panell',
   templateUrl: './left-panell.component.html',
   styleUrl: './left-panell.component.scss'
 })
-export class LeftPanellComponent {
+export class LeftPanellComponent implements OnInit {
+
+  playlists: IPlaylist[] = [];
 
   homeIcon = faHome;
   searchIcon = faSearch;
@@ -15,8 +19,19 @@ export class LeftPanellComponent {
 
   selectedMenu: string = 'Home';
 
+  constructor(private spofityService: SpotifyService) { }
+
+  ngOnInit(): void {
+    this.getPlaylists();
+  }
+
   buttonClick(description: string) {
     this.selectedMenu = description;
+  }
+
+  async getPlaylists() {
+    this.playlists = await this.spofityService.getUserPlaylist({ limit: 10 });
+    console.log(this.playlists);
   }
 
 }

8f3ee2cb9079bc413bf0ca167209119bca6a2b48
AugustoCVS <guty.vertolis@gmail.com>
2024-10-29 20:06:22 -0300

    feat: create handleNavigate

--- src/app/components/left-panell/left-panell.component.ts
+++ src/app/components/left-panell/left-panell.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { IPlaylist } from '../../interfaces/playlist';
 import { SpotifyService } from '../../services/spotify.service';
 import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-left-panell',
@@ -18,9 +19,12 @@ export class LeftPanellComponent implements OnInit {
   artistIcon = faGuitar;
   playlistIcon = faMusic;
 
-  selectedMenu: string = 'Home';
+  selectedMenu: string = 'home';
 
-  constructor(private spofityService: SpotifyService) { }
+  constructor(
+    private spofityService: SpotifyService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
     this.spofityService.getUserPlaylistFromSpotify({ offset: 0, limit: 20 });
@@ -29,10 +33,15 @@ export class LeftPanellComponent implements OnInit {
 
   buttonClick(description: string): void {
     this.selectedMenu = description;
+    this.handleNavigate({ screen: `player/${description}` });
   }
 
   getPlaylists(): void {
     this.playlists$ = this.spofityService.getUserPlaylistInfo();
+  }
+
+  handleNavigate({ screen }: { screen: string }): void {
+    this.router.navigate([screen]);
   }
 
 }

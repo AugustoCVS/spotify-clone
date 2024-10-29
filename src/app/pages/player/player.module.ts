beaac699831d1c1d59d5b6b9af889dc34c7a9637
AugustoCVS <guty.vertolis@gmail.com>
2024-10-29 14:01:19 -0300

    feat: import FontAwesomeModule

--- src/app/pages/player/player.module.ts
+++ src/app/pages/player/player.module.ts
@@ -5,6 +5,7 @@ import { RouterModule } from "@angular/router";
 import { playerRoutes } from "./player.routes";
 import { LeftPanellComponent } from "../../components/left-panell/left-panell.component";
 import { MenuButtonComponent } from "../../components/menu-button/menu-button.component";
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import { MenuButtonComponent } from "../../components/menu-button/menu-button.co
   ],
   imports: [
     CommonModule,
+    FontAwesomeModule,
     RouterModule.forChild(playerRoutes)
   ],
 })

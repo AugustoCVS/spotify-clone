21e092908c22266db3223bab72e1737aa77c9027
AugustoCVS <guty.vertolis@gmail.com>
2024-10-29 14:01:36 -0300

    feat: add description as a Input

--- src/app/components/menu-button/menu-button.component.ts
+++ src/app/components/menu-button/menu-button.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-menu-button',
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrl: './menu-button.component.scss'
 })
 export class MenuButtonComponent {
+  @Input()
+  description: string = '';
 
 }

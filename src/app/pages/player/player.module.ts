bb8286234e128f60e223ffae375c8ebce619ac11
AugustoCVS <guty.vertolis@gmail.com>
2024-10-29 13:27:26 -0300

    feat: create menu-button

--- src/app/pages/player/player.module.ts
+++ src/app/pages/player/player.module.ts
@@ -4,11 +4,13 @@ import { PlayerComponent } from "./player.component";
 import { RouterModule } from "@angular/router";
 import { playerRoutes } from "./player.routes";
 import { LeftPanellComponent } from "../../components/left-panell/left-panell.component";
+import { MenuButtonComponent } from "../../components/menu-button/menu-button.component";
 
 @NgModule({
   declarations: [
     PlayerComponent,
     LeftPanellComponent,
+    MenuButtonComponent
   ],
   imports: [
     CommonModule,

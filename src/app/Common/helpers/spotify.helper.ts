6ed4084e26fed01f2f8e7a7b4b5c766903bb41a4
AugustoCVS <guty.vertolis@gmail.com>
2024-10-29 14:44:39 -0300

    feat: create DefineSpotifyPlaylist

--- src/app/Common/helpers/spotify.helper.ts
+++ src/app/Common/helpers/spotify.helper.ts
@@ -1,3 +1,4 @@
+import { IPlaylist } from "../../interfaces/playlist";
 import { IUser } from "../../interfaces/user";
 
 export function DefineSpotifyUser({ user }: { user: SpotifyApi.CurrentUsersProfileResponse }): IUser {
@@ -5,5 +6,13 @@ export function DefineSpotifyUser({ user }: { user: SpotifyApi.CurrentUsersProfi
     id: user.id,
     name: user.display_name || '',
     imageUrl: user.images?.pop()?.url || ''
+  }
+}
+
+export function DefineSpotifyPlaylist({ playlist }: { playlist: SpotifyApi.PlaylistObjectSimplified }): IPlaylist {
+  return {
+    id: playlist.id,
+    name: playlist.name,
+    imageUrl: playlist.images?.pop()?.url || ''
   }
 }

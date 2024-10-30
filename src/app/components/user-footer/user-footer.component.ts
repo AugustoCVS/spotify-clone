bfda4761333805a1a4b696a7ed27a691c22774fe
AugustoCVS <guty.vertolis@gmail.com>
2024-10-29 21:29:05 -0300

    feat: add protected

--- src/app/components/user-footer/user-footer.component.ts
+++ src/app/components/user-footer/user-footer.component.ts
@@ -10,7 +10,7 @@ import { SpotifyService } from '../../services/spotify.service';
 })
 export class UserFooterComponent implements OnInit {
   logoutIcon = faSignOutAlt;
-  userInfo: IUser = {} as IUser;
+  protected userInfo: IUser = {} as IUser;
 
   constructor(private spofityService: SpotifyService) { }
 

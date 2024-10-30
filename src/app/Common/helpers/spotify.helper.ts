14399db15fcfd792e6a8fb1274c570ba4ddb5b05
AugustoCVS <guty.vertolis@gmail.com>
2024-10-30 11:32:24 -0300

    feat: create DefineSpotifyTrack helper

--- src/app/Common/helpers/spotify.helper.ts
+++ src/app/Common/helpers/spotify.helper.ts
@@ -1,6 +1,8 @@
 import { IArtist } from "../../interfaces/artists";
+import { IMusic } from "../../interfaces/music";
 import { IPlaylist } from "../../interfaces/playlist";
 import { IUser } from "../../interfaces/user";
+import { newArtist, newMusic, newPlaylist } from "../spotify.factories";
 
 export function DefineSpotifyUser({ user }: { user: SpotifyApi.CurrentUsersProfileResponse }): IUser {
   return {
@@ -11,6 +13,8 @@ export function DefineSpotifyUser({ user }: { user: SpotifyApi.CurrentUsersProfi
 }
 
 export function DefineSpotifyPlaylist({ playlist }: { playlist: SpotifyApi.PlaylistObjectSimplified }): IPlaylist {
+  if (!playlist) return newPlaylist();
+
   return {
     id: playlist.id,
     name: playlist.name || 'Sem Nome',
@@ -19,9 +23,30 @@ export function DefineSpotifyPlaylist({ playlist }: { playlist: SpotifyApi.Playl
 }
 
 export function DefineSpotifyArtist({ artist }: { artist: SpotifyApi.ArtistObjectFull }): IArtist {
+  if (!artist) return newArtist();
+
   return {
     id: artist.id,
     name: artist.name,
     imageUrl: artist.images?.sort((a, b) => a.width! - b.width!).pop()?.url || ''
+  }
+}
+
+export function DefineSpotifyTrack({ track }: { track: SpotifyApi.TrackObjectFull }): IMusic {
+  if (!track) return newMusic();
+
+  return {
+    id: track.id,
+    title: track.name,
+    album: {
+      id: track.album.id,
+      imgUrl: track.album.images?.sort((a, b) => a.width! - b.width!).pop()?.url || '',
+      name: track.album.name
+    },
+    artists: track.artists.map(artist => ({
+      id: artist.id,
+      name: artist.name
+    })),
+    time: ''
   }
 }

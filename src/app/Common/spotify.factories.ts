b800b59bbc20a323e46e33ecce469c4f38db9f4e
AugustoCVS <guty.vertolis@gmail.com>
2024-10-30 11:32:10 -0300

    feat: create spotifu factories

---
+++ src/app/Common/spotify.factories.ts
@@ -0,0 +1,34 @@
+import { IArtist } from "../interfaces/artists";
+import { IMusic } from "../interfaces/music";
+import { IPlaylist } from "../interfaces/playlist";
+
+
+export function newArtist(): IArtist {
+  return {
+    id: '',
+    imageUrl: '',
+    name: '',
+  };
+}
+
+export function newPlaylist(): IPlaylist {
+  return {
+    id: '',
+    imageUrl: '',
+    name: '',
+  }
+}
+
+export function newMusic(): IMusic {
+  return {
+    id: '',
+    album: {
+      id: '',
+      imgUrl: '',
+      name: '',
+    },
+    artists: [],
+    time: '',
+    title: '',
+  }
+}

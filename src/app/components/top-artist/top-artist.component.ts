433a032aa6d4df11cbaf5114952fd8256b032b15
AugustoCVS <guty.vertolis@gmail.com>
2024-10-29 21:29:18 -0300

    feat: implemeanting get top artists

--- src/app/components/top-artist/top-artist.component.ts
+++ src/app/components/top-artist/top-artist.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SpotifyService } from '../../services/spotify.service';
+import { Observable } from 'rxjs';
+import { IArtist } from '../../interfaces/artists';
 
 @Component({
   selector: 'app-top-artist',
   templateUrl: './top-artist.component.html',
   styleUrl: './top-artist.component.scss'
 })
-export class TopArtistComponent {
+export class TopArtistComponent implements OnInit {
+
+  protected topArtists$: Observable<IArtist[]> = new Observable<IArtist[]>();
+
+  constructor(private spotifyService: SpotifyService) { }
+
+  ngOnInit() {
+    this.spotifyService.getTopArtistsFromSpotify({ limit: 1 });
+    this.getTopArtists();
+  }
+
+  getTopArtists() {
+    this.topArtists$ = this.spotifyService.getTopArtistsInfo();
+    console.log(this.topArtists$);
+  }
 
 }

a8a3928fe7faef181b523919cb5dccc36ad458f4
AugustoCVS <guty.vertolis@gmail.com>
2024-10-30 11:41:03 -0300

    feat: create getSavedMusicsFromSpotify call

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -2,11 +2,12 @@ import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import Spotify from 'spotify-web-api-js'
 import { IUser } from '../interfaces/user';
-import { DefineSpotifyArtist, DefineSpotifyPlaylist, DefineSpotifyUser } from '../common/helpers/spotify.helper';
+import { DefineSpotifyArtist, DefineSpotifyPlaylist, DefineSpotifyTrack, DefineSpotifyUser } from '../common/helpers/spotify.helper';
 import { IPlaylist } from '../interfaces/playlist';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { Router } from '@angular/router';
 import { IArtist } from '../interfaces/artists';
+import { IMusic } from '../interfaces/music';
 
 @Injectable({
   providedIn: 'root'
@@ -17,6 +18,7 @@ export class SpotifyService {
   private spotifyApi!: Spotify.SpotifyWebApiJs;
   private playlists$: BehaviorSubject<IPlaylist[]> = new BehaviorSubject<IPlaylist[]>([]);
   private topArtists$: BehaviorSubject<IArtist[]> = new BehaviorSubject<IArtist[]>([]);
+  private savedMusics$: BehaviorSubject<IMusic[]> = new BehaviorSubject<IMusic[]>([]);
 
   constructor(private router: Router) {
     this.spotifyApi = new Spotify();
@@ -72,8 +74,8 @@ export class SpotifyService {
     localStorage.setItem('token', token);
   }
 
-  async getUserPlaylistFromSpotify({ offset = 0, limit = 50 }): Promise<void> {
-    await this.spotifyApi.getUserPlaylists(this.user.id, { offset, limit })
+  getUserPlaylistFromSpotify({ offset = 0, limit = 50 }): void {
+    this.spotifyApi.getUserPlaylists(this.user.id, { offset, limit })
       .then(playlists => {
         const formattedPlaylists = playlists.items.map((playlist) => DefineSpotifyPlaylist({ playlist }));
         this.playlists$.next(formattedPlaylists);
@@ -84,7 +86,7 @@ export class SpotifyService {
       });
   }
 
-  async getTopArtistsFromSpotify({ limit = 10 }) {
+  getTopArtistsFromSpotify({ limit = 10 }): void {
     this.spotifyApi.getMyTopArtists({ limit })
       .then(topArtists => {
         const formattedArtists = topArtists.items.map((artist) => DefineSpotifyArtist({ artist }));
@@ -96,12 +98,28 @@ export class SpotifyService {
       });
   }
 
+  getSavedMusicsFromSpotify({ offset = 0, limit = 50 }): void {
+    this.spotifyApi.getMySavedTracks({ offset, limit })
+      .then(savedMusics => {
+        const formattedMusics = savedMusics.items.map((music) => DefineSpotifyTrack({ track: music.track }));
+        this.savedMusics$.next(formattedMusics);
+      })
+      .catch(error => {
+        console.error('Error fetching saved musics', error);
+        this.savedMusics$.next([]);
+      });
+  }
+
   getUserPlaylistInfo(): Observable<IPlaylist[]> {
     return this.playlists$.asObservable();
   }
 
   getTopArtistsInfo(): Observable<IArtist[]> {
     return this.topArtists$.asObservable();
+  }
+
+  getsavedMusicsInfo(): Observable<IMusic[]> {
+    return this.savedMusics$.asObservable();
   }
 
   logout(): void {

1d8fadfc6b4b67e5ea919a895b518b4981a7248b
AugustoCVS <guty.vertolis@gmail.com>
2024-10-31 16:38:16 -0300

    feat: implemeanting spotify service

--- src/app/components/top/top.component.ts
+++ src/app/components/top/top.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SpotifyService } from '../../services/spotify.service';
+import { Observable } from 'rxjs';
+import { IArtist } from '../../interfaces/artists';
 
 @Component({
   selector: 'app-top',
   templateUrl: './top.component.html',
   styleUrl: './top.component.scss'
 })
-export class TopComponent {
+export class TopComponent implements OnInit {
+
+  protected topArtists$: Observable<IArtist[]> = new Observable<IArtist[]>();
+
+  constructor(private spotityService: SpotifyService) { }
+
+  ngOnInit(): void {
+    this.topArtists$ = this.spotityService.getTopArtistsInfo();
+  }
 
 }
